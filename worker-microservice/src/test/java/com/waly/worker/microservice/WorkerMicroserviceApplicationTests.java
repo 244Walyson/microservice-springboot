22982049636a973127018ca1e724633ed37843d4
walyson <walysonmoises1342@gmail.com>
2023-08-09 23:41:36 -0300

    project created

---
+++ worker-microservice/src/test/java/com/waly/worker/microservice/WorkerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waly.worker.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
