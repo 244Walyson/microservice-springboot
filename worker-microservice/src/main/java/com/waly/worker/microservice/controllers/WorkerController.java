4d0e942ffb2cfaef6bdf283ed4a1417074337833
walyson <walysonmoises1342@gmail.com>
2023-08-11 13:17:19 -0300

    hr-worker as config

--- worker-microservice/src/main/java/com/waly/worker/microservice/controllers/WorkerController.java
+++ worker-microservice/src/main/java/com/waly/worker/microservice/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import com.waly.worker.microservice.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,10 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private Environment env;
@@ -35,5 +40,11 @@ public class WorkerController {
     public ResponseEntity<Worker> findById(@PathVariable Long id){
         logger.info("PORT = " + env.getProperty(("local.server.port")));
         return ResponseEntity.ok(repository.findById(id).orElseThrow());
+    }
+
+    @GetMapping(value = "/config")
+    public ResponseEntity<Void> find(){
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
