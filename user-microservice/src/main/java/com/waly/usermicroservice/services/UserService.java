f236fba49d9dd76e9a4e840d8f638a4525281958
walyson <walysonmoises1342@gmail.com>
2023-08-11 23:12:47 -0300

    oauth project

--- user-microservice/src/main/java/com/waly/usermicroservice/services/UserService.java
+++ user-microservice/src/main/java/com/waly/usermicroservice/services/UserService.java
@@ -1,5 +1,6 @@
 package com.waly.usermicroservice.services;
 
+import com.waly.usermicroservice.entities.User;
 import com.waly.usermicroservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    private UserRepository respository;
+    private UserRepository repository;
+
+    public User findByEmail(String email){
+        return repository.findByEmail(email);
+    }
+
+    public User findById(Long id){
+        return repository.findById(id).orElseThrow();
+    }
 }
