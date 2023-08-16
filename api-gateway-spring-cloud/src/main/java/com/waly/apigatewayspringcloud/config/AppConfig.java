8f949122c7c20c436f8545073e61bbb2d10fa213
walyson <walysonmoises1342@gmail.com>
2023-08-15 21:29:10 -0300

    return to run in intellij

--- api-gateway-spring-cloud/src/main/java/com/waly/apigatewayspringcloud/config/AppConfig.java
+++ api-gateway-spring-cloud/src/main/java/com/waly/apigatewayspringcloud/config/AppConfig.java
@@ -1,4 +1,23 @@
 package com.waly.apigatewayspringcloud.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
 }
